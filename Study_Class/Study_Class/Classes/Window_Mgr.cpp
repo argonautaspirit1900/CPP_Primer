6cc9fda4215ea6cdacbc17db36d3bed2bc702e98
Xu Zan <victorywell2003@163.com>
2014-12-11 15:54:47 +0800

    增加一個新類 Window_Mgr, 添加其定義與實現的程序文件。

---
+++ Study_Class/Study_Class/Classes/Window_Mgr.cpp
@@ -0,0 +1,24 @@
+/***************************************************************************************************
+ * File name		: Window_Mgr.cpp
+ * Description		: Implement the class Window_Mgr
+ * Creator			: XU ZAN
+ * Creation date	: Thur.	Dec. 11, 2014
+ * Copyright(C)		2014	All rights reserved.
+ * 
+ ***************************************************************************************************/
+
+#include "Window_Mgr.h"
+
+
+Window_Mgr::Window_Mgr(void) 
+{
+}
+
+Window_Mgr::Window_Mgr(const Window_Mgr& orig) 
+{
+}
+
+Window_Mgr::~Window_Mgr(void) 
+{
+}
+
---
+++ Study_Class/Study_Class/Classes/Window_Mgr.h
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ * File name		: Window_Mgr.h
+ * Description		: Define the class Window_Mgr
+ * Creator			: XU ZAN
+ * Creation date	: Thur.	Dec. 11, 2014
+ * Copyright(C)		2014	All rights reserved.
+ * 
+ ***************************************************************************************************/
+
+#ifndef WINDOW_MGR_H
+#define	WINDOW_MGR_H
+
+	class Window_Mgr 
+	{
+	public:
+		Window_Mgr(void);
+		Window_Mgr(const Window_Mgr& orig);
+		virtual ~Window_Mgr(void);
+	private:
+	};
+
+#endif	/* WINDOW_MGR_H */
+
