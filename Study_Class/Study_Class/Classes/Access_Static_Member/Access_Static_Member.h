aa289e9842015123198a6c9c8c1f87859498c8e8
Xu Zan <victorywell2003@163.com>
2014-12-11 20:06:50 +0800

    對含有static成員的類的對象， 訪問其類中的static成員。 結果發現測試失敗， GCC編譯器編譯失敗。
    欲轉回Microsoft Visual Studio C++ 環境下去測試看看。

---
+++ Study_Class/Study_Class/Classes/Access_Static_Member/Access_Static_Member.h
@@ -0,0 +1,16 @@
+/***************************************************************************************************
+ * File name		: Access_Static_Member.h
+ * Description		: Study how to access the static member inside a class.
+ * Creator			: XU ZAN
+ * Creation date	: Thur.	Dec. 11, 2014
+ * Copyright(C)		2014	All rights reserved.
+ * 
+ ***************************************************************************************************/
+
+#ifndef ACCESS_STATIC_MEMBER_H
+#define	ACCESS_STATIC_MEMBER_H
+
+	void Utilize_Static_Class_member(void);
+
+#endif	/* ACCESS_STATIC_MEMBER_H */
+
